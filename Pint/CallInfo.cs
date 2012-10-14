c3cbecfa3572723a27f9eeea5bc9371797c33ba9
Edwin Young <edyoung@microsoft.com>
2012-10-13 18:33:37 -0700

    add functioncallvisitor

---
+++ Pint/CallInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation.Language;
+
+namespace Pint
+{
+    public class CallInfo
+    {
+        private CommandAst ast;
+
+        public CallInfo(CommandAst ast)
+        {
+            this.ast = ast;
+        }
+
+        public string Target
+        {
+            get
+            {
+                return ((StringConstantExpressionAst)ast.CommandElements[0]).Value;
+            }
+        }
+    }
+}
